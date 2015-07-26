6761d31cad35becf10ace4976dc3bd5d02f09287
Siddarta Jairam <sidjsb@gmail.com>
2015-07-26 01:16:41 -0500

    Added actual maping functions
    - idea was to make a bunch of sets of calls that would be made during the actual returned function call.
    - It should then be easy for someone else to add a new rule and make a more complex function call for the conversion. or reference flags in the dictionary.

--- R/convFunctionCalls.R
+++ R/convFunctionCalls.R
@@ -15,7 +15,7 @@ convFunctionsCalls <- function(linesMat, dict){
 	rArgs <- mapply(function(marg, fun){ fun(marg) }, matArgs, argMaps)
 
 	linesDes[potSet] <- getBetween(linesMat[potSet], '(', ')', rArgs)
-	
+
 	return(linesDes)
 }
 
@@ -25,21 +25,40 @@ makeMaps <- function(addDict = NULL, pathDict = ''){
 		if(!file.exists(pathDict)){
 			stop(paste(pathDict, "does not exist, please supply a dictionary file"))
 		}
-		
+
 		dictFile <- readLines(pathDict)
 		if (length(dictFile) == 0){
 			stop(paste(pathDict, "is empty, please fill with matLab functions"))
 		}
 		dictLines <- c(dictLines, dictFile)
-	} 
-	
+	}
+
 	if(length(dictLines) == 0){
 		stop(paste("No dictionaries supplied",
 			"either feed in a character vector"
 			"or a file with the dictionaries", sep = ", "))
 	}
 
-	
+	mapList <- list()
+	funNames <- getbetween(dictLines, "", ":")
+	mapFuns <- vapply(dictLines, function(lin){
+		sargs <- strsplit(strsplit(lin, ":")[[2]], ',')
+		sargs <- trimWhite(sargs)
 
+		swiSet <- grepl("^[0-9]+$", sargs)
+		literalNumSet <- grepl("^[0-9]+L$", sargs)
+		stringSet <- !literalNumSet & !swiSet
+
+		return(function(matArg){
+			rargs[swiSet] <- matArg[as.integer(sargs[swiSet])]
+			rargs[literalNumSet] <- as.numeric(sargs[literalNumSet])
+			rargs[stringSet] <- sargs[stringSet]
+
+			return(paste(rargs, sep = ", "))
+			})
+		}, function(x)cat(x))
+
+	mapList[[funNames]] <- mapFuns
+	return(mapList)
 
 }
