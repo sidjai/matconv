f3aba651004c69ef676f1d3a410570c333bab175
Siddarta Jairam <sidjsb@gmail.com>
2015-07-27 20:06:18 -0500

    Made makeMaps exported + work
    - ending function call made a list in a list when what I wanted was just to set the names.
    - R doesn't like L in as.numeric
    - strsplit outputs a list instead of a vector

--- R/convFunctionCalls.R
+++ R/convFunctionCalls.R
@@ -14,11 +14,19 @@ convFunctionsCalls <- function(linesMat, dict){
 
 	rArgs <- mapply(function(marg, fun){ fun(marg) }, matArgs, argMaps)
 
-	linesDes[potSet] <- getBetween(linesMat[potSet], '(', ')', rArgs)
+	linesDes[potSet] <- getBetween(linesMat[potSet], '- ', ')', rArgs)
 
 	return(linesDes)
 }
 
+#' Turn dictionary lines into functions that map matlab to R function calls
+#'
+#' @param addDict An optional character vector with manufactored lines
+#' @param pathDict The path to a text file with the dictionry lines written to it
+#'
+#' @return a list of functions with names of matlab functions and the function
+#' to change the matlab input into an R function call
+#' @export
 makeMaps <- function(addDict = NULL, pathDict = ''){
 	dictLines <- addDict
 	if(nzchar(pathDict)){
@@ -35,30 +43,41 @@ makeMaps <- function(addDict = NULL, pathDict = ''){
 
 	if(length(dictLines) == 0){
 		stop(paste("No dictionaries supplied",
-			"either feed in a character vector"
+			"either feed in a character vector",
 			"or a file with the dictionaries", sep = ", "))
 	}
 
 	mapList <- list()
-	funNames <- getbetween(dictLines, "", ":")
-	mapFuns <- vapply(dictLines, function(lin){
-		sargs <- strsplit(strsplit(lin, ":")[[2]], ',')
-		sargs <- trimWhite(sargs)
+	keyVal <- strsplit(dictLines, ":")
+	funNames <- vapply(keyVal, function(x){ x[1] }, "e")
+	funVals <- vapply(keyVal, function(x){ x[2] }, "e")
+
+	mapFuns <- lapply(dictLines, function(lin){
+		sargs <- strsplit(funVals, ',')
+		sargs <- trimWhite(sargs[[1]])
+
+		rname <- sargs[1]
+		sargs <- sargs[-1]
 
 		swiSet <- grepl("^[0-9]+$", sargs)
 		literalNumSet <- grepl("^[0-9]+L$", sargs)
 		stringSet <- !literalNumSet & !swiSet
 
 		return(function(matArg){
+			rargs <- NULL
 			rargs[swiSet] <- matArg[as.integer(sargs[swiSet])]
-			rargs[literalNumSet] <- as.numeric(sargs[literalNumSet])
+			rargs[literalNumSet] <- as.numeric(gsub("L", "", sargs[literalNumSet]))
 			rargs[stringSet] <- sargs[stringSet]
 
-			return(paste(rargs, sep = ", "))
+			return(paste0(rname, '(', paste(rargs, collapse = ", ")))
 			})
-		}, function(x)cat(x))
+		})
 
-	mapList[[funNames]] <- mapFuns
-	return(mapList)
+	names(mapFuns) <- funNames
+	return(mapFuns)
 
+}
+
+`isKey` <- function(vals, ldict){
+	return(is.element(names(ldict), vals))
 }
